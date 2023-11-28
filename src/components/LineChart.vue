7b6deea676fe91327b83ae20988f1fae8a25db01
Pierp23 <ryu.no.seishin.21@gmail.com>
2023-11-28 14:03:10 +0100

    ADD line chart

---
+++ src/components/LineChart.vue
@@ -0,0 +1,75 @@
+<script>
+import axios from 'axios';
+import { Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+
+ChartJS.register(CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend)
+
+
+
+export default {
+  name: 'LineChart',
+  components: { Line },
+  data() {
+    return {
+      loaded: false,
+      chartData: '',
+      chartOptions: {
+        responsive: true
+      }
+    }
+  },
+  methods: {
+    getDataJson() {
+      this.loaded = false,
+        axios.get('http://localhost:8080/' + 'Pierp23/FakeStats/db')
+          .then(response => {
+            // handle success
+            console.log(response.data.MonthlyConnections);
+            // this.ageRange = response.data.MonthlyConnections
+            this.chartData = {
+              labels: response.data.MonthlyConnections.map(item => item.month),
+              datasets: [{
+                label: 'Connessioni mensili',
+                backgroundColor: '#f87979',
+                data: response.data.MonthlyConnections.map(item => item.connections)
+              }]
+            }
+            this.loaded = true
+            console.log(this.chartData)
+          })
+          .catch(function (error) {
+            // handle error
+            console.log(error);
+          })
+    }
+  },
+  created() {
+    this.getDataJson();
+  }
+}
+</script>
+
+<template>
+  <Line v-if="loaded" id="my-chart-id" :options="chartOptions" :data="chartData" />
+</template>
+  
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
+  
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="main">
+    <div>
+      <h2>
+        Grafico 1
+      </h2>
+      <LineChart />
+    </div>
+    <hr>
+    <div>
+      <h2>
+        Grafico 2
+      </h2>
+      <DoughnutChart />
+    </div>
+    <hr>
+    <div>
+      <h2>
+        Grafico 3
+      </h2>
+      <BarChart />
+    </div>
+  </div>
+</template>
+
+<script>
+import LineChart from './LineChart.vue';
+import DoughnutChart from './DoughnutChart.vue'
+import BarChart from './BarChart.vue';
+
+
+export default {
+  components: {
+    LineChart,
+    DoughnutChart,
+    BarChart
+
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main {
+  width: 1000px;
+  height: 100%;
+  padding: 10px;
+  margin: 0 auto;
+}
+</style>
